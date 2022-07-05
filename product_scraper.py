68f7e082c97ff74d7daad2f0b2d3e2979c7c399f
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-07-05 16:40:08 +0800

    Improved script such that its output is now a readable file (json format) Moved handling of request outside open file Used json library to format data extracted from request Add filter to return only desired data Changed write file to json format

--- product_scraper.py
+++ product_scraper.py
@@ -1,14 +1,28 @@
 import requests
 import re
-
+import json
 
 url = 'https://www.lazada.com.ph/catalog/?from=input&q='
 query = 'laptop&location=Local&rating=4'
 # search = input('Search item in Lazada: ')
 
 txt = requests.get(url + query).text
-# txt.replace("\","")
-with open('./data/product_search/product_lazada.txt','w') as w:
-    script = re.findall(r'"listItems":(.*)', txt)
-    str_scr = ''.join(script)
-    print(str_scr, file=w)
+script = re.findall(r'"listItems":(.*),"breadcrumb"', txt)
+str_scr = ''.join(script)
+products_data = json.loads(str_scr)
+filtered_data = []
+
+for product in products_data:
+    s = {
+            "Name": product['name'],
+            "RatingScore": product['ratingScore'],
+            "Review": product['review'],
+            "SellerId": product['sellerId'],
+            "SellerName": product['sellerName'],
+        }
+    filtered_data.append(s)
+
+json_data = json.dumps(filtered_data, indent = 4)
+
+with open('./data/product_search/product_lazada.json','w') as w:
+    w.write(json_data)
