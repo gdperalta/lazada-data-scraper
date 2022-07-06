5b90137dd88693b16533427e36d09262e003df12
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-07-06 08:53:28 +0800

    Add option to convert to csv For refactoring

--- product_scraper.py
+++ product_scraper.py
@@ -14,8 +14,16 @@ Created: July 4, 2022
 import requests  # handles communication with api
 import re        # handles regex matches
 import json      # handles conversion to json
+import csv
 
 URL = 'https://www.lazada.com.ph/catalog/?from=input&q='
+FIELD_NAMES = [
+                'seller_name',
+                'seller_id',
+                'product_name',
+                'rating_score',
+                'reviews'
+              ]
 query = 'laptop&location=Local&rating=4'
 PRODUCTS_INFO = []
 # search = input('Search item in Lazada: ')
@@ -24,7 +32,8 @@ PRODUCTS_INFO = []
 def main():
     data = scrape_data()
     filter_data(data)
-    download_data()
+    # download_to_json()
+    download_to_csv()
 
 
 def scrape_data():
@@ -39,20 +48,27 @@ def scrape_data():
 def filter_data(data):
     for d in data:
         filtered_data = {
-            "SellerName": d['sellerName'],
-            "SellerId": d['sellerId'],
-            "ProductName": d['name'],
-            "RatingScore": d['ratingScore'],
-            "Review": d['review'],
+            "seller_name": d['sellerName'],
+            "seller_id": d['sellerId'],
+            "product_name": d['name'],
+            "rating_score": d['ratingScore'],
+            "reviews": d['review'],
         }
 
         PRODUCTS_INFO.append(filtered_data)
 
 
-def download_data():
+def download_to_json():
     data = json.dumps(PRODUCTS_INFO, indent=4)
     with open('./data/product_search/product_lazada.json', 'w') as w:
         w.write(data)
+
+
+def download_to_csv():
+    with open('./data/product_search/product_lazada.csv', 'w', encoding='UTF8', newline='') as w:
+        data = csv.DictWriter(w, fieldnames=FIELD_NAMES)
+        data.writeheader()
+        data.writerows(PRODUCTS_INFO)
 
 
 if __name__ == "__main__":
